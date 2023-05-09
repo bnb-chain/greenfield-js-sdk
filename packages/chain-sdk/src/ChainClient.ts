7ee318de75405744256b220fdb0e8e56a125e030
rrr523 <gaoyibobo@foxmail.com>
2023-05-09 15:28:43 +0800

    feat: Account API

--- packages/chain-sdk/src/ChainClient.ts
+++ packages/chain-sdk/src/ChainClient.ts
@@ -1,5 +1,10 @@
+import { Account, IAccount } from './api/account';
+import { Basic, IBasic } from './api/basic';
+
 export class ChainClient {
-  static create(): ChainClient {
-    return new ChainClient();
+  static create(rpcUrl: string, chainId: string): ChainClient {
+    return new ChainClient(new Account(rpcUrl, chainId), new Basic(rpcUrl, chainId));
   }
+
+  constructor(public account: IAccount, public basic: IBasic) {}
 }
