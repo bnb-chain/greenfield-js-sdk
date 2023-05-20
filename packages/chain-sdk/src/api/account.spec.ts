c579c980abccfc4919c089c4ac9eca3b13f3882b
rrr523 <gaoyibobo@foxmail.com>
2023-05-20 17:10:52 +0800

    test: Account Query

--- packages/chain-sdk/src/api/account.spec.ts
+++ packages/chain-sdk/src/api/account.spec.ts
@@ -4,10 +4,56 @@ import { Client } from '../client';
 
 const client = Client.create(GRPC_URL, GREENFIELD_CHAIN_ID);
 
-describe('getAccount', () => {
-  test('works', async () => {
-    const res = await client.account.getAccount(ACCOUNT.address);
+describe('accountQuery', () => {
+  describe('getAccount', () => {
+    test('it works', async () => {
+      const res = await client.account.getAccount(ACCOUNT.address);
 
-    expect(res).not.toBeNull();
+      expect(res).not.toBeNull();
+    });
+  });
+
+  describe('getAccountBalance', () => {
+    test('it works', async () => {
+      const res = await client.account.getAccountBalance({
+        address: ACCOUNT.address,
+        denom: 'BNB',
+      });
+
+      expect(res).not.toBeNull();
+    });
+  });
+
+  describe('getPaymentAccount', () => {
+    test('it works', async () => {
+      const res = await client.account.getPaymentAccount({
+        addr: ACCOUNT.address,
+      });
+
+      expect(res).not.toBeNull();
+    });
+  });
+
+  describe('getModuleAccounts', () => {
+    test('it works', async () => {
+      const res = await client.account.getModuleAccounts();
+
+      expect(res).not.toBeNull();
+    });
+  });
+
+  // TODO: don't work
+  // describe('getPaymentAccountsByOwner', () => {
+  //   test('it works', async () => {
+  //     const res = await client.account.getPaymentAccountsByOwner(ACCOUNT.address);
+
+  //     expect(res).not.toBeNull();
+  //   });
+  // });
+});
+
+describe('accountTx', () => {
+  describe('transfer', async () => {
+    // ...
   });
 });
