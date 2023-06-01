41305539d3e117bd3f398b9f31b6b494e7323feb
rrr523 <59029880+rrr523@users.noreply.github.com>
2023-06-01 22:18:34 +0800

    chore: Update example without greenfield-cosmos-types (#71)

--- examples/nextjs/src/utils/simulate.ts
+++ examples/nextjs/src/utils/simulate.ts
@@ -1,22 +1,9 @@
-import { SimulateResponse } from '@bnb-chain/greenfield-cosmos-types/cosmos/tx/v1beta1/service';
-import { QueryParamsResponse } from '@bnb-chain/greenfield-cosmos-types/greenfield/bridge/query';
 import { ethers } from 'ethers';
 
-export const getGasFeeBySimulate = (simulateTxInfo: SimulateResponse) => {
-  if (!simulateTxInfo.gasInfo) throw new Error('gasInfo not found');
-
-  const gasLimit = simulateTxInfo.gasInfo?.gasUsed.toNumber();
-  const gasPrice = simulateTxInfo.gasInfo?.minGasPrice.replaceAll('BNB', '');
-  const gasFee = gasLimit * parseInt(gasPrice);
-
-  return ethers.utils.formatEther(String(gasFee));
-};
-
-export const getRelayFeeBySimulate = (simulateRelayFeeInfo: QueryParamsResponse) => {
-  if (!simulateRelayFeeInfo.params) return '0';
-
-  const { transferOutRelayerFee, transferOutAckRelayerFee } = simulateRelayFeeInfo.params;
-
+export const getRelayFeeBySimulate = (
+  transferOutRelayerFee: string,
+  transferOutAckRelayerFee: string,
+) => {
   const relayFee = ethers.utils.parseUnits(transferOutRelayerFee, 0);
   const ackFee = ethers.utils.parseUnits(transferOutAckRelayerFee, 0);
 
