402da22ea101f855a05f098b8970eef13b045ead
rrr523 <59029880+rrr523@users.noreply.github.com>
2023-08-30 19:10:05 +0800

    fix: Convert enum to number (#287)

--- packages/chain-sdk/src/clients/spclient/spApis/getBucketMeta.ts
+++ packages/chain-sdk/src/clients/spclient/spApis/getBucketMeta.ts
@@ -1,4 +1,5 @@
 import { GetBucketMetaResponse } from '@/types';
+import { formatBucketInfo } from '@/types/sp-xml/Common';
 import { XMLParser } from 'fast-xml-parser';
 
 // https://docs.bnbchain.org/greenfield-docs/docs/api/storgae-provider-rest/get_bucket_meta
@@ -7,6 +8,14 @@ export const parseGetBucketMetaResponse = async (data: string) => {
     parseTagValue: false,
   });
   const res = xmlParser.parse(data) as GetBucketMetaResponse;
+
+  res.GfSpGetBucketMetaResponse.Bucket = {
+    ...res.GfSpGetBucketMetaResponse.Bucket,
+    BucketInfo: formatBucketInfo(res.GfSpGetBucketMetaResponse.Bucket.BucketInfo),
+    DeleteAt: Number(res.GfSpGetBucketMetaResponse.Bucket.DeleteAt),
+    UpdateAt: Number(res.GfSpGetBucketMetaResponse.Bucket.UpdateAt),
+    UpdateTime: Number(res.GfSpGetBucketMetaResponse.Bucket.UpdateTime),
+  };
 
   return res;
 };
