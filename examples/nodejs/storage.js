6d94a485dd3c415018ab2d9a0124978804937ca5
rrr523 <gaoyibobo@foxmail.com>
2023-09-08 15:08:02 +0800

    chore: Nodejs example update

---
+++ examples/nodejs/storage.js
@@ -0,0 +1,45 @@
+const {
+  client,
+  selectSp,
+  ACCOUNT_ADDRESS,
+  ACCOUNT_PRIVATEKEY,
+  generateString,
+} = require('./client');
+
+(async () => {
+  const bucketName = generateString(10);
+  const spInfo = await selectSp();
+  const createBucketTx = await client.bucket.createBucket(
+    {
+      bucketName: bucketName,
+      creator: ACCOUNT_ADDRESS,
+      visibility: 'VISIBILITY_TYPE_PUBLIC_READ',
+      chargedReadQuota: '0',
+      spInfo: {
+        primarySpAddress: spInfo.primarySpAddress,
+      },
+      paymentAddress: ACCOUNT_ADDRESS,
+    },
+    {
+      type: 'ECDSA',
+      privateKey: ACCOUNT_PRIVATEKEY,
+    },
+  );
+
+  const simulateInfo = await createBucketTx.simulate({
+    denom: 'BNB',
+  });
+
+  console.log('simulateInfo', simulateInfo);
+
+  const res = await createBucketTx.broadcast({
+    denom: 'BNB',
+    gasLimit: Number(simulateInfo?.gasLimit),
+    gasPrice: simulateInfo?.gasPrice || '5000000000',
+    payer: ACCOUNT_ADDRESS,
+    granter: '',
+    privateKey: ACCOUNT_PRIVATEKEY,
+  });
+
+  console.log('res', res);
+})();
