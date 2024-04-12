61476ef473bf8a46ed8098d41c0015cb2db1ea16
rrr523 <gaoyibobo@foxmail.com>
2024-04-12 12:06:19 +0800

    chore: Cross BSC and Gnfd init

---
+++ packages/bsc-cross-sdk/rollup.config.js
@@ -0,0 +1,85 @@
+import alias from '@rollup/plugin-alias';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+
+import autoExternal from 'rollup-plugin-auto-external';
+import pkg from './package.json';
+const pathResolve = (p) => path.resolve(__dirname, p);
+
+function resolveExternal() {
+  return [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+}
+
+export default async () => {
+  return [
+    {
+      input: './src/index.ts',
+      output: {
+        dir: './dist/esm',
+        format: 'esm',
+        sourcemap: true,
+      },
+      external: resolveExternal(),
+      context: 'window',
+      treeshake: true,
+      plugins: [
+        json({
+          include: ['src/**'],
+        }),
+        // builtins(),
+        resolve({
+          exportConditions: ['default', 'module', 'import'],
+          mainFields: ['module', 'main'],
+          modulesOnly: true,
+          preferBuiltins: false,
+        }),
+        commonjs({
+          defaultIsModuleExports: false,
+        }),
+        typescript({
+          tsconfig: './config/tsconfig-esm.json',
+          declarationDir: './dist/esm',
+        }),
+        alias({
+          entries: {
+            '@': pathResolve('src'),
+          },
+        }),
+      ],
+    },
+    {
+      input: './src/index.ts',
+      output: {
+        dir: './dist/cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      external: resolveExternal(),
+      plugins: [
+        json({
+          include: ['src/**'],
+        }),
+        autoExternal(),
+        nodePolyfills({
+          include: 'node_modules/**',
+        }),
+        commonjs({
+          ignoreDynamicRequires: true,
+        }),
+        typescript({
+          tsconfig: './config/tsconfig-cjs.json',
+          declarationDir: './dist/cjs/types',
+        }),
+        alias({
+          entries: {
+            '@': pathResolve('src'),
+          },
+        }),
+      ],
+    },
+  ];
+};
